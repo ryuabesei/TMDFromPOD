38e38aa9389aa4001256cefb37757e9443b08bc0
ryuabesei <yayuyo.sei8@gmail.com>
2026-02-05 14:14:47 +0900

    datainfo確認

--- datainfo.py
+++ datainfo.py
@@ -1,9 +1,10 @@
 from cdflib import CDF
 
-path = "SW_OPER_DNSCPOD_2__20180201T000000_20180201T235930_0301.cdf"
+path = "data/SWARM_C/SW_OPER_DNSCPOD_2__20180201T000000_20180201T235930_0301.cdf"
 cdf = CDF(path)
 
 info = cdf.cdf_info()
-print("zVariables:", info["zVariables"])
-print("rVariables:", info["rVariables"])
-print("Attributes:", info["Attributes"])
+
+print("zVariables:", info.zVariables)
+print("rVariables:", info.rVariables)
+print("Attributes:", info.Attributes)
